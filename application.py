baef0cf8d7d2113d2e52a1a95b2527c61a68056f
R4phC <r4phael@gmail.com>
2016-05-14 17:57:05 +0100

    Fixing port issue

--- application.py
+++ application.py
@@ -131,4 +131,6 @@ def backend(destination,activities,knownactivities):
     return (finalresult, '')
     
 if __name__ == "__main__":
-    application.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    application.run(host='0.0.0.0', port=port)
